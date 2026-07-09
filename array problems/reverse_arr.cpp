09064f10ba9ce24a0668738b3cd80dfef2c7993d
tiktiki <itzpomodoro79@gmail.com>
2026-07-09 21:12:23 +0600

    Implement array reversal using two-pointer technique

---
+++ array problems/reverse_arr.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// two pointer approach
+int main(){
+    int arr[] = {1, 2, 4, 5, 3, 8};
+    int size = 6;
+
+    int i = 0;
+    int j = size - 1;
+
+    while( i <= j){
+        swap(arr[i], arr[j]);
+        i++;
+        j--;
+    }
+
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
