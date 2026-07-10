f6ca594c4743ec315c48becb3773229104a2f7eb
tiktiki <itzpomodoro79@gmail.com>
2026-07-10 23:15:37 +0600

    add vector related problem solving files

---
+++ vector problems/single_number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> nums = {4, 1, 3, 4, 1};
+
+    int ans = 0;
+    for(int i : nums){
+        ans ^= i; // XOR operation to find the single number
+    }
+
+    cout << ans;
+    
+    return 0;
+}
---
+++ vector problems/single_number2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> nums = {3, 2, 3, 3, 1, 2, 2};
+    int ans = 0;
+    for(int n : nums){
+        
+    }
+    cout << ans;
+
+    return 0;
+}
