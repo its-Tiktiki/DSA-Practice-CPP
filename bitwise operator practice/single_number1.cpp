083514e535620b1362f0de9b087e2b88307adb0c
tiktiki <itzpomodoro79@gmail.com>
2026-07-11 13:00:51 +0600

    update single_number1.cpp file

--- bitwise operator practice/single_number1.cpp
+++ bitwise operator practice/single_number1.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    vector<int> nums = {3, 2, 3, 3, 1, 2, 2};
+    vector<int> nums = {3, 2, 4, 3, 1, 4, 2};
 
     int ans = 0;
     for(int i : nums){
