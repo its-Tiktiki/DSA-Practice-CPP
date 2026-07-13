22a606275f21f3b094580e1b5c33db4a6076616e
tiktiki <itzpomodoro79@gmail.com>
2026-07-13 20:51:20 +0600

    update print statement

--- array practice/twoD_arr.cpp
+++ array practice/twoD_arr.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
     int rows, cols;
+    cout << "Enter rows number and columns number: ";
     cin >> rows >> cols;
     int arr[rows][cols];
 
@@ -11,7 +12,7 @@ int main(){
         }
     }
 
-    cout << "The matrix is : " << endl;
+    cout << "The 2D array is : " << endl;
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < cols; j++){
             cout << arr[i][j] << " ";
