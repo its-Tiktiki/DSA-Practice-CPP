2603ab7ee8230b9bbd45c31725c960f21820f833
its-Tiktiki <itzpomodoro79@gmail.com>
2026-07-31 20:24:29 +0600

    add selection sort implementation code

---
+++ sorting practice/selection_sort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i <n; ++i){
+        cin >> a[i];
+    }
+
+    for(int i = 0; i < n; ++i){
+        int min_indx = i;
+        for(int j = i + 1; j < n; ++j){
+            if(a[j] < a[min_indx]){
+                min_indx = j;
+            }
+        }
+        swap(a[i], a[min_indx]);
+    }
+
+    for(int i = 0; i < n; ++i){
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
