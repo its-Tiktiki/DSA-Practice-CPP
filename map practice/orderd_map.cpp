f2290d65b55b48fac9d3c892f231e24fb7609e12
its-Tiktiki <itzpomodoro79@gmail.com>
2026-08-02 14:05:28 +0600

    add practice file of ordered map

--- map practice/orderd_map.cpp
+++ map practice/orderd_map.cpp
@@ -13,8 +13,27 @@ int main(){
     m[2] = "ilu";
     m[3] = "idgaf";
     m.insert({4, "fu"});
-
     print(m);
+
+    auto it = m.find(3);
+    if(it == m.end()){
+        cout << "No value\n";
+    }
+    else{
+        cout << (it->first) << " " << (it->second) << endl;
+    }
+
+    it = m.find(7);
+    if(it == m.end()){
+        cout << "No value\n";
+    }
+    else{
+        cout << (it->first) << " " << (it->second) << endl;
+    }
+
+    m.erase(1);
+    print(m);
+
 
     return 0;
 }
