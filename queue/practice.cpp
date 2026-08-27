d1e81a504873c9fde5b3cfa59d2b67270f0f242b
its-Tiktiki <itzpomodoro79@gmail.com>
2026-08-27 22:43:20 +0600

    update quque practice file

--- queue/practice.cpp
+++ queue/practice.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+
     queue<string> q;
     q.push("arpa");
-    q.push("apra");
+    q.push("apa");
     q.push("ara");
     q.push("papa");
     while(!q.empty()){
