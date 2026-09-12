a107588b6dfae3cebffba83c0e3d5d0aeb66af1b
its-Tiktiki <itzpomodoro79@gmail.com>
2026-09-12 20:35:01 +0600

    update left corner triangle printing file

--- pattern printing/left_corner_triangle.cpp
+++ pattern printing/left_corner_triangle.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-
     int nums;
     cout << "enter the number of rows: ";
     cin >> nums;
 
     string star = "*";
-
     for(int i = 0; i < nums; i++){
         cout << star;
         star += " *";
         cout << endl;
 
     }
-
 
     return 0;
 }
