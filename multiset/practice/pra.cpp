d4c51d6606e1a957540b27e71610d14fdf1b6bb5
its-Tiktiki <itzpomodoro79@gmail.com>
2026-09-15 22:35:15 +0600

    typo fix

--- multiset/practice/pra.cpp
+++ multiset/practice/pra.cpp
@@ -1,30 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print(multiset<string> &s){
-    for(auto value: s){
-        cout << value << endl;
-    }
-}
-int main(){
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    // can store duplicate element
-    multiset<string> s;
-    s.insert("abc");
-    s.insert("sdf");
-    s.insert("ghj");
-
-    // only erase the value that the iterator points
-    // mainly the first value of the given string
-    auto it = s.find("abc");
-    if(it != s.end()){
-        cout << (*it) << endl;
-        s.erase(it);
-    }
-
-    // erases all the string that matches the given string 
-    s.erase("sdf");
-    print(s);
+    
 
     return 0;
 }
