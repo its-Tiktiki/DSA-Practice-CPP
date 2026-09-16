09e1daf3e8d592471b4d0980738c5c00ede5d3fe
its-Tiktiki <itzpomodoro79@gmail.com>
2026-09-16 23:10:53 +0600

    add new file

--- function practice/pass_by_reference.cpp
+++ function practice/pass_by_reference.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int A = 7; // gobal variable
+// gobal variable
+int A = 7;
 void change(){
     A++;
 }
 
 int main(){
-    // int a = 7; // value wont change bcz its a local variable
+    // int a = 7; //value wont change bcz its a local variable
     cout << "Before change a = " << A << endl;
     change();
     cout << "After change a = " << A << endl;
