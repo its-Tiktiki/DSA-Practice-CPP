5c465df51d99a4893e244caf7d031ad25643a7bf
its-Tiktiki <itzpomodoro79@gmail.com>
2026-09-20 17:41:05 +0600

    update file

--- function practice/pass_by_reference.cpp
+++ function practice/pass_by_reference.cpp
@@ -8,7 +8,7 @@ void change(){
 }
 
 int main(){
-    // int a = 7; //value wont change bcz its a local variable
+    // int a = 7; // value wont change bcz its a local variable
     cout << "Before change a = " << A << endl;
     change();
     cout << "After change a = " << A << endl;
