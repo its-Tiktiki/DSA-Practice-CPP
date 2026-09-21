565d3afcaec94c592c2e829071a2b6df124345cd
its-Tiktiki <itzpomodoro79@gmail.com>
2026-09-21 19:01:19 +0600

    add 1D array practice file

---
+++ array/practice/oneD_arr.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    // int a[10];
+    // a[11] = 10; // its an undefined behaviour
+
+    int a[n];
+    for(int i = 0; i < n; ++i){
+        cin >> a[i];
+    }
+
+    return 0;
+}
